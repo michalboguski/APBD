a9dc494d30f603a7aba2972998ad1c56d526a779
s25236@pjwstk.edu.pl <s25236@pjwstk.edu.pl>
2024-05-11 21:43:12 +0200

    add new branch CW4 for cw4 excercise

---
+++ cw4 Warehouse/WarehouseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw4
+{
+    [Route("/warehouse")]
+    internal class WarehouseController : ControllerBase
+    {
+        long productId { get; set; }
+        long warehouseId { get; set; }
+        int amount { get; set; }
+        DateTime createdAt { get; set; }
+
+
+    }
+
+
+
+
+}
