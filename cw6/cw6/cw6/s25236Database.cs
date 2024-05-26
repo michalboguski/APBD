37edeebec10a5f08c15d2145ffd9523eaa5823e9
Michał Boguski <s25236@pjwstk.edu.pl>
2024-05-26 15:23:00 +0200

    add dbContext

---
+++ cw6/cw6/cw6/s25236Database.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cw6;
+
+public class s25236Database : DbContext
+{
+    public s25236Database()
+    {
+    }
+    public s25236Database(DbContextOptions<s25236Database> options) :base(options)
+    {
+    }
+}
